9b1bc4bede2de416601fa979eed6ace53df14d5c
Jacob Barton <j.barton.mail@googlemail.com>
2018-04-25 15:56:38 +0100

    Chat and AIML files
    Added chat to HTML and CSS.
    Created AIML files.

--- backup/run.py
+++ backup/run.py
@@ -1,53 +1,36 @@
 from flask import Flask, render_template, g
 import time
 
-
 app = Flask(__name__)
 
+# Render index page
 @app.route('/')
 def main():
-    return render_template('home.html')
+    return render_template('index.html')
 
-@app.route('/home_form', methods=['GET'])
-def home_form():
-    return render_template('home.html', methods=['GET'])
+@app.route("/ask", methods=['POST','GET'])
+def ask():
+	# Gather input
+	message = str(request.form['chatmessage'])
 
-@app.route('/four_form')
-def four_form():
-    return render_template('4x4.html', methods=['GET'])
+	kernel = aiml.Kernel()
 
-@app.route('/saloon_form')
-def saloon_form():
-    return render_template('Saloon.html', methods=['GET'])
+	# Ready kernel
+	if os.path.isfile("connor_brain.brn"):
+	    kernel.bootstrap(brainFile = "connor_brain.brn")
+	else:
+	    kernel.bootstrap(learnFiles = os.path.abspath("aiml/start.xml"), commands = "load aiml")
+	    kernel.saveBrain("connor_brain.brn")
 
-@app.route('/hatchback_form')
-def hatchback_form():
-    return render_template('Hatchback.html', methods=['GET'])
+	# Get and return response
+	while True:
+	    if message == "quit":
+	        break
+	    else:
+	        bot_response = kernel.respond(message)
 
-@app.route('/convertible_form')
-def convertible_form():
-    return render_template('Convertible.html', methods=['GET'])
-
-@app.route('/estate_form')
-def estate_form():
-    return render_template('Estate.html', methods=['GET'])
-
-@app.route('/coupe_form')
-def coupe_form():
-    return render_template('Coupe.html', methods=['GET'])
-
-@app.route('/contact_form')
-def contact_form():
-    return render_template('Contact.html', methods=['GET'])
-
-@app.route('/login_form')
-def login_form():
-    return render_template('login.html', methods=['GET'])
-
-@app.before_request
-def before_request():
-    g.request_start_time = time.time()
-    g.request_time = lambda: "%.5fs" % (time.time() - g.request_start_time)
-
+			
+	        return jsonify({'status':'OK','answer':bot_response})
+            
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
--- run.py
+++ run.py
@@ -6,35 +6,39 @@ import sqlite3
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template('chat.html')
+def main():
+    return render_template('index.html')
 
 @app.route("/ask", methods=['POST','GET'])
 def ask():
+    # Initiate connection with database
     with sqlite3.connect(r"C:\Users\Dave\Desktop\connor.db") as conn:
         c = conn.cursor()
 
+    # Preper kernel
     kernel = aiml.Kernel()
 
-    if os.path.isfile("bot_brain.brn"):
-	    kernel.bootstrap(brainFile = "bot_brain.brn")
+    if os.path.isfile("connor_brain.brn"):
+	    kernel.bootstrap(brainFile = "connor_brain.brn")
     else:
-	    kernel.bootstrap(learnFiles = os.path.abspath("aiml/std-startup.xml"), commands = "load aiml b")
-	    kernel.saveBrain("bot_brain.brn")
+	    kernel.bootstrap(learnFiles = os.path.abspath("aiml/start.xml"), commands = "load aiml")
+	    kernel.saveBrain("connor_brain.brn")
     
+    # Get response from HTML/JS
     bot_response = kernel.respond(str(request.form['chatmessage']))
     
+    # Get variables and test by printing
     colour = kernel.getPredicate("colour")
     model = kernel.getPredicate("model")
     print colour
     print model
 
+    # Query databse and test by printing
     c.execute("SELECT Make, Price FROM CarTable WHERE Colour = '%s' AND Model = '%s'" % (colour, model))
     results = c.fetchall()
-
     print results
 
-    # print bot_response
+    # Return response
     return jsonify({'status':'OK','answer':bot_response})
 
 if __name__ == "__main__":
